18e51954afcefe9cf3c5575a96f57d82dcbb9270
김찬희 <blackeagle10@icloud.com>
2025-01-19 18:46:41 +0900

    add render-markdown toggle keymap

--- .config/nvim/lua/josean/plugins/render-markdown.lua
+++ .config/nvim/lua/josean/plugins/render-markdown.lua
@@ -4,4 +4,13 @@ return {
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
 	opts = {},
+	keys = {
+		{
+			"<leader>rm",
+			function()
+				require("render-markdown").toggle()
+			end,
+			desc = "Toggle render-markdown plugin",
+		},
+	},
 }
