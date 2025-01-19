62f0692127c5f704ae527ffce488abfe31ece844
김찬희 <blackeagle10@icloud.com>
2025-01-19 12:33:24 +0900

    remove keys from floaterm

--- .config/nvim/lua/josean/plugins/floaterm.lua
+++ .config/nvim/lua/josean/plugins/floaterm.lua
@@ -7,11 +7,6 @@ return {
 			vim.g.floaterm_height = 0.8 -- Set floaterm height (as a fraction of the screen)
 			vim.g.floaterm_position = "center" -- Set floaterm position
 			vim.g.floaterm_autoclose = 1 -- Automatically close floaterm when process exits
-
-			-- Keybindings
-			vim.keymap.set("n", "<leader>fo", ":FloatermToggle<CR>", { desc = "Open Floaterm" })
-			vim.keymap.set("n", "<leader>fn", ":FloatermNew<CR>", { desc = "New Floaterm" })
-			vim.keymap.set("n", "<leader>fk", ":FloatermKill<CR>", { desc = "Kill Floaterm" })
 		end,
 	},
 }
