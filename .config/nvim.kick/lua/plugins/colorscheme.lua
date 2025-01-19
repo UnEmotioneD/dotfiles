df5eb0ded65adc204e3f7e1c5fa513b1d39e930c
김찬희 <blackeagle10@icloud.com>
2025-01-20 08:55:21 +0900

    .ideavimrc turn of sound

--- .config/nvim.kick/lua/plugins/colorscheme.lua
+++ .config/nvim.kick/lua/plugins/colorscheme.lua
@@ -132,8 +132,8 @@ return {
 --   lazy = false, -- We want the colorscheme to load immediately when starting Neovim
 --   priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
 --   opts = {
---   --   -- Replace this with your scheme-specific settings or remove to use the defaults
---     -- transparent = true,
+--   -- Replace this with your scheme-specific settings or remove to use the defaults
+--   -- transparent = true,
 --     flavour = "mocha", -- "latte, frappe, macchiato, mocha"
 --   },
 --   config = function(_, opts)
