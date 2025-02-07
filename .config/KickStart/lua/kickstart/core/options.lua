f1940acb80205d01e16c5032fa28e73d07374f9b
김찬희 <blackeagle10@icloud.com>
2025-02-08 01:42:03 +0900

    update kickstart.nvim config

--- .config/KickStart/lua/kickstart/core/options.lua
+++ .config/KickStart/lua/kickstart/core/options.lua
@@ -57,11 +57,6 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 -- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
