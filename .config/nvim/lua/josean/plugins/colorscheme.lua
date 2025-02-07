d834cbc188bfb92196a9eb2c387d1dccc198d550
김찬희 <blackeagle10@icloud.com>
2025-02-08 01:22:00 +0900

    update colorscheme.lua

--- .config/nvim/lua/josean/plugins/colorscheme.lua
+++ .config/nvim/lua/josean/plugins/colorscheme.lua
@@ -3,10 +3,8 @@ local tokyonight = {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup({
-			style = "storm", -- storm(default) | moon | night | day
-		})
-		vim.cmd([[colorscheme tokyonight]])
+		-- storm(default) | moon | night | day
+		vim.cmd([[colorscheme tokyonight-storm]])
 	end,
 }
 
