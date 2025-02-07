8d4f27aec41ba2abbb49cd2008fd7a90f778c298
김찬희 <blackeagle10@icloud.com>
2025-02-07 11:49:35 +0900

    add catppuccin to kitty and nvim

--- .config/nvim/lua/josean/plugins/colorscheme.lua
+++ .config/nvim/lua/josean/plugins/colorscheme.lua
@@ -1,11 +1,27 @@
-return {
+local tokyonight = {
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
 		require("tokyonight").setup({
-			style = "storm", -- storm(default), moon, night, day
+			style = "storm", -- storm(default) | moon | night | day
 		})
 		vim.cmd([[colorscheme tokyonight]])
 	end,
 }
+
+local catppuccin = {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte | frappe | macchiato | mocha
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+}
+
+return tokyonight
