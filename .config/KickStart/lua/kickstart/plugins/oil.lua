82a729bff6780484d7ec99d9ed65f4485fe09928
김찬희 <blackeagle10@icloud.com>
2025-02-08 13:54:39 +0900

    show hidden file in oil.nvim KickStart.nvim

--- .config/KickStart/lua/kickstart/plugins/oil.lua
+++ .config/KickStart/lua/kickstart/plugins/oil.lua
@@ -6,6 +6,12 @@ return {
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   lazy = false,
-
+  config = function()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
+  end,
   vim.keymap.set('n', '-', '<cmd>Oil<CR>', { desc = 'Open parent directory' }),
 }
