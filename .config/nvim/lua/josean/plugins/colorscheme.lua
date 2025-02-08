e8289f4612a898a0aa2c8b39605202246d12b9a2
김찬희 <blackeagle10@icloud.com>
2025-02-08 12:54:25 +0900

    remove catppuccin theme

--- .config/nvim/lua/josean/plugins/colorscheme.lua
+++ .config/nvim/lua/josean/plugins/colorscheme.lua
@@ -1,25 +1,8 @@
-local tokyonight = {
+return {
 	"folke/tokyonight.nvim",
-	lazy = false,
 	priority = 1000,
-	config = function()
-		-- storm(default) | moon | night | day
-		vim.cmd([[colorscheme tokyonight-storm]])
+	init = function()
+		vim.cmd.colorscheme("tokyonight-storm")
+		vim.cmd.hi("Comment gui=none")
 	end,
 }
-
-local catppuccin = {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha", -- latte | frappe | macchiato | mocha
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-}
-
-return tokyonight
