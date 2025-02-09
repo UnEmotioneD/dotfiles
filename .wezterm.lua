15c8c98c2c6b08c6fecc8ab3c706d37a63b06412
김찬희 <blackeagle10@icloud.com>
2025-02-10 01:24:24 +0900

    refactor wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,28 +2,25 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback({
-	{ family = "Fira Code", weight = "Bold" },
-	{ family = "MesloLGS Nerd Font Mono", weight = "Bold" }, -- fallback font
-})
-config.font_size = 16
+config = {
+	automatically_reload_config = true,
+	font = wezterm.font("Fira Code", { weight = "Bold" }),
+	font_size = 16,
+	-- enable ligature
+	harfbuzz_features = { "calt=1", "liga=1" },
 
--- enable ligature
-config.harfbuzz_features = { "calt=1", "liga=1" }
+	enable_tab_bar = false,
 
-config.enable_tab_bar = false
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.85,
+	window_padding = {
+		left = 15,
+		right = 15,
+		top = 15,
+		bottom = 15,
+	},
 
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.85
-config.window_padding = {
-	left = 15,
-	right = 15,
-	top = 15,
-	bottom = 15,
+	color_scheme = "Tokyo Night Storm",
 }
-
-config.macos_window_background_blur = 20
-
-config.color_scheme = "Tokyo Night Storm"
 
 return config
