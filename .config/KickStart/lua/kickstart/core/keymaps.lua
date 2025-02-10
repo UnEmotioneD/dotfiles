2bd4963947f47e2e1df60ab07909bae4eeeede1e
김찬희 <blackeagle10@icloud.com>
2025-02-10 13:28:18 +0900

    move some keys from Josean.nvim to KickStart.nvim

--- .config/KickStart/lua/kickstart/core/keymaps.lua
+++ .config/KickStart/lua/kickstart/core/keymaps.lua
@@ -15,3 +15,18 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Unbind keymaps to avoid collision with tmux keybinds
+vim.keymap.set('', '<C-n>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('', '<C-p>', '<Nop>', { noremap = true, silent = true })
+
+-- Center window when navigating through search
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
+
+-- Write / Quit
+vim.keympa.set('n', '<leader>ww', '<cmd>w<CR>', { desc = 'Write current file' })
+vim.keympa.set('n', '<leader>wa', '<cmd>wa<CR>', { desc = 'Write all files' })
+vim.keympa.set('n', '<leader>qq', '<cmd>q<CR>', { desc = 'Quite current buffer' })
+vim.keympa.set('n', '<leader>qa', '<cmd>qa<CR>', { desc = 'Quite all buffers' })
+vim.keympa.set('n', '<leader>wq', '<cmd>wqa<CR>', { desc = 'Write and quite all buffers' })
