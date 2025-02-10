4854d98b48c316694041228b504720837680f3fe
김찬희 <blackeagle10@icloud.com>
2025-02-10 19:03:34 +0900

    change stylua column to 80 and indent to 4

--- .config/KickStart/lua/kickstart/plugins/oil.lua
+++ .config/KickStart/lua/kickstart/plugins/oil.lua
@@ -1,20 +1,25 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- Optional
-  lazy = false,
-  config = function()
-    require('oil').setup {
-      view_options = {
-        show_hidden = true,
-        is_always_hidden = function(name, bufnr)
-          local dsStore = name:match '.DS_Store'
-          return dsStore
-        end,
-      },
-    }
-  end,
-  vim.keymap.set('n', '-', '<cmd>Oil<CR>', { desc = 'Open parent directory' }),
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- Optional
+    lazy = false,
+    config = function()
+        require('oil').setup {
+            view_options = {
+                show_hidden = true,
+                is_always_hidden = function(name, bufnr)
+                    local dsStore = name:match '.DS_Store'
+                    return dsStore
+                end,
+            },
+        }
+    end,
+    vim.keymap.set(
+        'n',
+        '-',
+        '<cmd>Oil<CR>',
+        { desc = 'Open parent directory' }
+    ),
 }
