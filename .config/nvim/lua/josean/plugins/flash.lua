b2fa34c9827657c8d975ed87640713382205e20d
김찬희 <blackeagle10@icloud.com>
2025-02-10 10:45:33 +0900

    cleanup flash.nvim

--- .config/nvim/lua/josean/plugins/flash.lua
+++ .config/nvim/lua/josean/plugins/flash.lua
@@ -5,44 +5,12 @@ return {
 
 	keys = {
 		{
-			"<leader><leader>f",
-			mode = { "n", "x", "o" },
+			"<leader>fl",
+			mode = { "n" },
 			function()
 				require("flash").jump()
 			end,
 			desc = "Flash",
-		},
-		{
-			"<leader><leader>F",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
-			"<leader><leader>r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-		{
-			"<leader><leader>R",
-			mode = { "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
-			desc = "Treesitter Search",
-		},
-		{
-			"<c-s>",
-			mode = { "c" },
-			function()
-				require("flash").toggle()
-			end,
-			desc = "Toggle Flash Search",
 		},
 	},
 }
