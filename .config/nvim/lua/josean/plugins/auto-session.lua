b3bdb8df0267389fbaaea4aac1f4045a2506052d
김찬희 <blackeagle10@icloud.com>
2025-02-11 23:01:47 +0900

    remove some dir from auto_session_suppress

--- .config/nvim/lua/josean/plugins/auto-session.lua
+++ .config/nvim/lua/josean/plugins/auto-session.lua
@@ -2,6 +2,6 @@ return {
     'rmagatti/auto-session',
     opts = {
         auto_restore_enabled = false,
-        auto_session_suppress_dirs = { '~/', '~/Desktop/', '~/Documents/', '~/Downloads/', '~/Developer/' },
+        auto_session_suppress_dirs = { '~/', '~/Desktop/', '~/Downloads/' },
     },
 }
