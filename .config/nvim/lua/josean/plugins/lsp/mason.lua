d6a4aa8fa8aa16079ff389e63baea0e8c9661fbc
김찬희 <blackeagle10@icloud.com>
2025-02-11 22:11:27 +0900

    refactoring Josean.nvim

--- .config/nvim/lua/josean/plugins/lsp/mason.lua
+++ .config/nvim/lua/josean/plugins/lsp/mason.lua
@@ -6,9 +6,7 @@ return {
     },
     config = function()
         local mason = require 'mason'
-
         local mason_lspconfig = require 'mason-lspconfig'
-
         local mason_tool_installer = require 'mason-tool-installer'
 
         mason.setup {
@@ -32,7 +30,6 @@ return {
                 -- "clangd",
                 -- "rust_analyzer",
             },
-
             -- To silence "Missing required fields" warnings
             automatic_installation = true,
         }
