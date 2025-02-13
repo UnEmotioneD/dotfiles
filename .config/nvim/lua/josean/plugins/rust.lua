6c98b7951c34a90060f73655431df7b81318a72e
김찬희 <blackeagle10@icloud.com>
2025-02-13 17:44:25 +0900

    Cleanup rustaceanvim.lua file's comments

--- .config/nvim/lua/josean/plugins/rust.lua
+++ .config/nvim/lua/josean/plugins/rust.lua
@@ -2,17 +2,15 @@ return {
   -- Supports LSP for Rust
   {
     'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
-    lazy = false, -- Lazy by default
+    version = '^5',
+    lazy = false,
     ft = 'rust',
     config = function()
       local mason_registry = require 'mason-registry'
       local codelldb = mason_registry.get_package 'codelldb'
       local extension_path = codelldb:get_install_path() .. '/extension/'
       local codelldb_path = extension_path .. 'adapter/codelldb'
-      local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
-      -- If you are on Linux, replace the line above with the line below:
-      -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib' -- For Linux 'lldb/lib/liblldb.so'
       local cfg = require 'rustaceanvim.config'
 
       vim.g.rustaceanvim = {
