01a8b04d8969e910896d80311d24bd457c50e41e
UnEmotioneD <blackeagle10@icloud.com>
2025-02-19 02:10:56 +0900

    Update harpoon keybind && add lazy to alpha.lua

--- nvim/.config/nvim/lua/josean/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/josean/plugins/harpoon.lua
@@ -34,7 +34,7 @@ return {
 
     for i = 1, 4 do
       table.insert(keys, {
-        '<leader><leader>' .. i,
+        '<leader>' .. i,
         function()
           require('harpoon'):list():select(i)
         end,
