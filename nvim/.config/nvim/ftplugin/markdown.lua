a01e8b02b7f768581b89118e461c10f865cbf6fa
mac <blackeagle10@icloud.com>
2025-02-20 01:16:33 +0900

    Feature : add line wrap and acording keymaps to ftplugins/markdown.lua

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,8 +1,40 @@
-vim.keymap.set('n', '<leader>mp', '<Cmd>MarkdownPreview<CR>', { desc = '[m]arkdown [p]erview' })
+local opt = vim.opt_local
+local m = vim.keymap.set
 
-vim.keymap.set('n', '<leader>mr', function()
+-- Enable word wrap and break lines at word boundaries
+opt.wrap = true
+opt.linebreak = true
+
+-- Set a comfortable text width for automatic formatting (adjust as needed)
+opt.textwidth = 80
+opt.formatoptions:append('t') -- Automatically wrap text using textwidth
+
+-- Use conceal to hide certain Markdown syntax markers (e.g., emphasis markers)
+opt.conceallevel = 2
+opt.concealcursor = 'n' -- Only conceal when not in insert mode
+
+-- Indentation settings for lists and nested elements
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.expandtab = true
+
+-- Optional: Configure folding based on Markdown headers
+-- This simple expression folds sections starting at Markdown headers (e.g., "#", "##", etc.)
+opt.foldmethod = 'expr'
+opt.foldexpr = "getline(v:lnum)=~'^\\s*#' ? '>' . (strlen(submatch(0)) - 1) : '='"
+
+m('n', 'j', 'gj')
+m('n', 'k', 'gk')
+m('n', '<Down>', 'gj')
+m('n', '<Up>', 'gk')
+
+-- Keymaps for render, preview and open in obsidian
+m('n', '<leader>mp', '<Cmd>MarkdownPreview<CR>', { desc = '[m]arkdown [p]erview' })
+
+m('n', '<leader>mr', function()
   require('render-markdown').toggle()
 end, { desc = '[m]arkdown [r]ender' })
 
 -- Obsidian
-vim.keymap.set('n', '<leader>mo', '<Cmd>ObsidianOpen<CR>', { desc = '[m]arkdown in [o]bsidian' })
+m('n', '<leader>mo', '<Cmd>ObsidianOpen<CR>', { desc = '[m]arkdown in [o]bsidian' })
