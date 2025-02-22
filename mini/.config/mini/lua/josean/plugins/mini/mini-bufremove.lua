bd73f334452763344a5b9f8cb80156ea72d8eb23
mac <blackeagle10@icloud.com>
2025-02-22 16:01:27 +0900

    mini : add bufremove
    mini.tabline opens new file to new tab automatically
    but when :q<Cr> it will close every tabs
    so bufremove is needed and keymap to execute the required fucntion

---
+++ mini/.config/mini/lua/josean/plugins/mini/mini-bufremove.lua
@@ -0,0 +1,11 @@
+return {
+  'echasnovski/mini.bufremove',
+  version = '*',
+  opts = {
+    -- Whether to set Vim's settings for buffers (allow hidden buffers)
+    set_vim_settings = true,
+
+    -- Whether to disable showing non-error feedback
+    silent = false,
+  },
+}
