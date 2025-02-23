197366093069f87af92c97c7dba30c9ad85c142b
mac <blackeagle10@icloud.com>
2025-02-23 12:03:14 +0900

    Feature(color) : add catppuccin config of tmux to dotfiles also nvim and ghostty
    nvim colorscheme will show tokyongith to mac and catppuccin to other os

--- nvim/.config/nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/josean/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
-return {
+local tokyo = {
   'folke/tokyonight.nvim',
   priority = 1000,
   init = function()
@@ -11,3 +11,29 @@ return {
     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#7F849C' })
   end,
 }
+
+local cat = {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  config = function()
+    require('catppuccin').setup({
+      ---@type string | 'auto' | 'latte' | 'frappe' | 'macchiato' | 'mocah'
+      flavour = 'mocha',
+      transparent_background = false,
+    })
+    vim.cmd.colorscheme('catppuccin')
+
+    -- Color for line numbers (from vscode catppuccin-mocha line number)
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#7f849c' })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#7F849C' })
+  end,
+}
+
+local os_name = vim.loop.os_uname().sysname
+
+if os_name == 'Darwin' then
+  return tokyo
+else
+  return cat
+end
