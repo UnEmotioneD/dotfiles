c8ef73582a4c8b11b17b44fbc25e3172c73cc430
mac <unemotioned@MacBook.local>
2025-03-02 01:35:07 +0900

    Update(color) : add coolnight colorscheme to neovim, lualine and wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -13,6 +13,7 @@ config = {
 
 	window_decorations = "RESIZE",
 	window_background_opacity = 0.85,
+	macos_window_background_blur = 20,
 	window_padding = {
 		left = 15,
 		right = 15,
@@ -22,8 +23,19 @@ config = {
 
 	max_fps = 240,
 
-	---@type string | "Tokyo Night Storm" | "Catppuccin Mocha"
-	color_scheme = "Tokyo Night Storm",
+	-- Coolnight
+	colors = {
+		foreground = "#CBE0F0",
+		background = "#011423",
+		cursor_bg = "#47FF9C",
+		cursor_border = "#47FF9C",
+		cursor_fg = "#011423",
+		selection_bg = "#033259",
+		selection_fg = "#CBE0F0",
+		ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+		brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	},
+	-- color_scheme = "Tokyo Night Storm",
 }
 
 return config
