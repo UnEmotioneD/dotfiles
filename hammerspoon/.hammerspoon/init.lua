b5c9b4eac2da75dc1db40e2f7dd300f4151694cd
UnEmotioneD <blackeagle10@icloud.com>
2025-05-30 18:55:25 +0900

    Add hammerspoon

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,18 @@
+-- key mapping for vim
+-- Convert input source as English and sends 'escape' if inputSource is not English.
+-- Sends 'escape' if inputSource is English.
+-- key bindding reference --> https://www.hammerspoon.org/docs/hs.hotkey.html
+local inputEnglish = "com.apple.keylayout.ABC"
+local esc_bind
+
+function TO_ENG()
+	local inputSource = hs.keycodes.currentSourceID()
+	if not (inputSource == inputEnglish) then
+		hs.keycodes.currentSourceID(inputEnglish)
+	end
+	esc_bind:disable()
+	hs.eventtap.keyStroke({}, "escape")
+	esc_bind:enable()
+end
+
+esc_bind = hs.hotkey.new({}, "escape", TO_ENG):enable()
