4e641a52189e269d49e66a33edca5db0f50c76f6
UnEmotioneD <blackeagle10@icloud.com>
2025-06-09 13:49:42 +0900

    Remove(nvim): vague.nvim

--- nvim/.config/nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/josean/plugins/colorscheme.lua
@@ -10,53 +10,14 @@ return {
         comments = { italic = false },
         keywords = { italic = false },
       },
-      on_highlights = function(hl, c)
+      on_highlights = function(hl)
         local vague = '#5f607b'
         hl.Comment.fg = vague
         hl.DiagnosticUnnecessary.fg = vague
         hl.LineNrAbove.fg = vague
         hl.LineNrBelow.fg = vague
-
-        local prompt = '#2d3149'
-        hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
-        hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
-        hl.TelescopePromptNormal = { bg = prompt }
-        hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
-        hl.TelescopePromptTitle = { bg = prompt, fg = prompt }
-        hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
-        hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
       end,
     })
-
-    require('vague').setup({
-      style = {
-        boolean = 'none',
-        error = 'none',
-        comments = 'none',
-        headings = 'none',
-        strings = 'none',
-
-        keyword_return = 'none',
-
-        builtin_constants = 'none',
-        builtin_types = 'none',
-      },
-      plugins = {
-        cmp = {
-          match = 'none',
-          match_fuzzy = 'none',
-        },
-        lsp = {
-          diagnostic_info = 'none',
-          diagnostic_warn = 'none',
-          diagnostic_error = 'none',
-        },
-        telescope = {
-          match = 'none',
-        },
-      },
-    })
-
-    vim.cmd.colorscheme('vague')
+    vim.cmd.colorscheme('tokyonight')
   end,
 }
