40a35a164cedfacf9754b1e8cbf8942b70d635bf
UnEmotioneD <blackeagle10@icloud.com>
2025-07-14 19:46:36 +0900

    Fix(nvim): markdown-preview - add cmd and build field

--- nvim/.config/nvim/lua/josean/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/josean/plugins/markdown-preview.lua
@@ -1,6 +1,8 @@
 return {
   'iamcco/markdown-preview.nvim',
   ft = 'markdown',
+  cmd = 'MarkdownPreview',
+  build = 'cd app && yarn install',
   init = function()
     vim.g.mkdp_filetypes = { 'markdown' }
   end,
