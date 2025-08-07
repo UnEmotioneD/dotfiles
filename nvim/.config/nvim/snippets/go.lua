e6aa5d411de63c492865a1c9fa9000ef51eb4ed8
UnEmotioneD <blackeagle10@icloud.com>
2025-08-07 14:34:04 +0900

    add(nvim): luasnip - go snippests for main and normal func

---
+++ nvim/.config/nvim/snippets/go.lua
@@ -0,0 +1,35 @@
+local ls = require('luasnip')
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require('luasnip.extras.fmt').fmt
+
+return {
+  s(
+    'main',
+    fmt(
+      [[
+      package main
+
+      import "fmt"
+
+      func main() {{
+        {}
+      }}
+      ]],
+      {
+        i(0),
+      }
+    )
+  ),
+  s(
+    'func',
+    fmt(
+      [[
+    func {}({}) {} {{
+      {}
+    }}
+    ]],
+      { i(1, 'name'), i(2), i(3), i(0) }
+    )
+  ),
+}
