28ada4b6d5b17278a4ebb0bd9312aef9a783e6a3
UnEmotioneD <blackeagle10@icloud.com>
2025-08-27 17:14:06 +0900

    update(nvim): lazygit - scaling to 1

--- nvim/.config/nvim/lua/josean/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/josean/plugins/lazygit.lua
@@ -2,6 +2,7 @@ return {
   'kdheepak/lazygit.nvim',
   event = 'VeryLazy',
   config = function()
+    vim.g.lazygit_floating_window_scaling_factor = 1
     vim.keymap.set('n', '<leader>hg', ':LazyGit<CR>', { desc = 'LazyGit' })
   end,
 }
