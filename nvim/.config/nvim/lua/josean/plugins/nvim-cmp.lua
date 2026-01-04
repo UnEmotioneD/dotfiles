88affe1b41a010dbfce68ecf6c46ea05d107fed2
UnEmotioneD <blackeagle10@icloud.com>
2026-01-04 09:57:26 +0900

    update(nvim): cmp - config file

--- nvim/.config/nvim/lua/josean/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/josean/plugins/nvim-cmp.lua
@@ -1,19 +1,22 @@
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
-    'hrsh7th/cmp-nvim-lsp', -- LSP source for cmp
-    'hrsh7th/cmp-buffer', -- source for text in buffer
-    'hrsh7th/cmp-cmdline', -- source for nvim cmds
-    'hrsh7th/cmp-path', -- source for file system paths
-    'onsails/lspkind.nvim', -- vs-code like pictograms
-    'rafamadriz/friendly-snippets', -- useful snippets
-    'saadparwaiz1/cmp_luasnip', -- for autocompletion
+    'hrsh7th/cmp-nvim-lsp', -- LSP completion source
+    'hrsh7th/cmp-buffer', -- words in buffer
+    'hrsh7th/cmp-cmdline', -- nvim commands
+    'hrsh7th/cmp-path', -- filesystem paths
+    'onsails/lspkind.nvim', -- vscode style icons
+    'rafamadriz/friendly-snippets', -- prebuilt snippets
+    'saadparwaiz1/cmp_luasnip', -- snippet engine integration
   },
+
   config = function()
     local cmp = require('cmp')
     local luasnip = require('luasnip')
+    local lspkind = require('lspkind')
+    local kinds = require('cmp.types').lsp.CompletionItemKind
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    -- Load vscode style snippets
     require('luasnip.loaders.from_vscode').lazy_load()
 
     cmp.setup({
@@ -23,7 +26,7 @@ return {
       performance = {
         max_view_entries = 10,
       },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
+      snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
@@ -32,63 +35,66 @@ return {
         completion = cmp.config.window.bordered({ border = 'single' }),
         documentation = cmp.config.window.bordered({ border = 'single' }),
       },
+
       mapping = cmp.mapping.preset.insert({
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- accept first suggestion
         ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        ['<UP>'] = cmp.config.disable,
-        ['<DOWN>'] = cmp.config.disable,
+        -- disable selecting with arrow keys
+        ['<Up>'] = cmp.config.disable,
+        ['<Down>'] = cmp.config.disable,
       }),
-      -- sources for autocompletion
+
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' }, -- custom snippets
-        { name = 'buffer' }, -- text within current buffer
-        { name = 'path' }, -- file system paths
-        { name = 'render-markdown' }, -- add suggestions for markdown
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = 'render-markdown' }, -- source from marksman
       }),
 
-      format = function(entry, vim_item)
-        -- De-duplicate common sources
-        if entry.source.name == 'nvim_lsp' or entry.source.name == 'buffer' then
-          vim_item.dup = 0
-        end
+      formatting = {
+        format = function(entry, vim_item)
+          -- De-duplicate common sources
+          if entry.source.name == 'nvim_lsp' or entry.source.name == 'buffer' then
+            vim_item.dup = nil
+          end
 
-        -- Use Treesitter to detect if inside function args, and adjust display accordingly
-        local node = require('nvim-treesitter.ts_utils').get_node_at_cursor()
-        if node and node:type() == 'arguments' and (entry.completion_item.kind or 0) ~= 6 then
-          vim_item.menu = '[...]' -- visually mark low-priority suggestions
-        end
+          -- Use Treesitter to detect if inside function args, and adjust display accordingly
+          local node = vim.treesitter.get_node()
+          if node and node:type() == 'arguments' and entry:get_kind() ~= kinds.Function then
+            vim_item.menu = '[…]'
+          end
 
-        -- Add VS Code-style icons and trim long items
-        return require('lspkind').cmp_format({
-          maxwidth = 50,
-          ellipsis_char = '…',
-        })(entry, vim_item)
-      end,
+          -- Add icons + trim long entries
+          return lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = '…',
+          })(entry, vim_item)
+        end,
+      },
 
       sorting = {
-        priority_weight = 2,
+        priority_weight = 2, -- increasee weight than default
         comparators = {
-          cmp.config.compare.offset,
-          cmp.config.compare.exact,
-          cmp.config.compare.score,
+          cmp.config.compare.offset, -- cursor position
+          cmp.config.compare.exact, -- exact matches
+          cmp.config.compare.score, -- fuzzy match score
+
           -- Prioritize functions when inside function arguments
-          function(entry1, entry2)
-            local kind1 = entry1:get_kind()
-            local kind2 = entry2:get_kind()
-            if kind1 == 6 and kind2 ~= 6 then
+          function(e1, e2)
+            if e1:get_kind() == kinds.Function and e2:get_kind() ~= kinds.Function then
               return true
             end
-            if kind2 == 6 and kind1 ~= 6 then
+            if e2:get_kind() == kinds.Function and e1:get_kind() ~= kinds.Function then
               return false
             end
           end,
+          -- Built-in fallback comparisons
           cmp.config.compare.kind,
           cmp.config.compare.sort_text,
           cmp.config.compare.length,
@@ -97,7 +103,7 @@ return {
       },
     })
 
-    -- Snippets for search or directory
+    -- Search completion
     cmp.setup.cmdline({ '/', '?' }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
@@ -105,7 +111,7 @@ return {
       },
     })
 
-    -- Snippets for cmd
+    -- Command mode completion
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
