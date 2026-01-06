59bd37d5ccf34c768adc39f4b07e31a3d01e5496
UnEmotioneD <blackeagle10@icloud.com>
2026-01-07 08:20:50 +0900

    refactor(nvim): colortheme - vageu into dependencies

--- nvim/.config/nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/josean/plugins/colorscheme.lua
@@ -1,62 +1,53 @@
 return {
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    enabled = false,
-    config = function()
-      require('tokyonight').setup({
-        style = 'storm',
-        styles = {
-          comments = { italic = false },
-          keywords = { italic = false },
+  'folke/tokyonight.nvim',
+  dependencies = { 'vague-theme/vague.nvim' },
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('tokyonight').setup({
+      style = 'storm',
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+      },
+      on_highlights = function(hl)
+        local vague = '#5f607b'
+        hl.Comment.fg = vague
+        hl.DiagnosticUnnecessary.fg = vague
+        hl.LineNrAbove.fg = vague
+        hl.LineNrBelow.fg = vague
+      end,
+    })
+
+    require('vague').setup({
+      bold = true,
+      italic = true,
+      style = {
+        boolean = 'bold',
+        error = 'bold',
+        comments = 'italic',
+        headings = 'bold',
+        strings = 'italic',
+        keyword_return = 'italic',
+        builtin_constants = 'bold',
+        builtin_types = 'bold',
+      },
+      plugins = {
+        cmp = {
+          match = 'bold',
+          match_fuzzy = 'bold',
         },
-        on_highlights = function(hl)
-          local vague = '#5f607b'
-          hl.Comment.fg = vague
-          hl.DiagnosticUnnecessary.fg = vague
-          hl.LineNrAbove.fg = vague
-          hl.LineNrBelow.fg = vague
-        end,
-      })
-      vim.cmd.colorscheme('tokyonight')
-    end,
-    endabled = false,
-  },
-  {
-    'vague-theme/vague.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('vague').setup({
-        bold = true,
-        italic = true,
-        style = {
-          boolean = 'bold',
-          error = 'bold',
-          comments = 'italic',
-          headings = 'bold',
-          strings = 'italic',
-          keyword_return = 'italic',
-          builtin_constants = 'bold',
-          builtin_types = 'bold',
+        lsp = {
+          diagnostic_error = 'bold',
+          diagnostic_info = 'italic',
+          diagnostic_warn = 'bold',
         },
-        plugins = {
-          cmp = {
-            match = 'bold',
-            match_fuzzy = 'bold',
-          },
-          lsp = {
-            diagnostic_error = 'bold',
-            diagnostic_info = 'italic',
-            diagnostic_warn = 'bold',
-          },
-          telescope = {
-            match = 'bold',
-          },
+        telescope = {
+          match = 'bold',
         },
-      })
-      vim.cmd.colorscheme('vague')
-    end,
-  },
+      },
+    })
+
+    vim.cmd.colorscheme('tokyonight')
+  end,
 }
