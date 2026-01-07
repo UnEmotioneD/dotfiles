0c6d464130689b7e44c608d97af21f63716a6d6d
UnEmotioneD <blackeagle10@icloud.com>
2026-01-07 19:42:25 +0900

    Revert "remove(nvim): vague theme"
    This reverts commit 8a23c18207aeaed5863e9dc782b444bee795af57.

--- nvim/.config/nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/josean/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
   'folke/tokyonight.nvim',
+  dependencies = { 'vague-theme/vague.nvim' },
   lazy = false,
   priority = 1000,
   config = function()
     require('tokyonight').setup({
       style = 'storm',
+      transparent = true,
       styles = {
         comments = { italic = false },
         keywords = { italic = false },
@@ -17,6 +19,36 @@ return {
         hl.LineNrBelow.fg = vague
       end,
     })
+
+    require('vague').setup({
+      bold = true,
+      italic = true,
+      style = {
+        boolean = 'bold',
+        error = 'bold',
+        comments = 'italic',
+        headings = 'bold',
+        strings = 'italic',
+        keyword_return = 'italic',
+        builtin_constants = 'bold',
+        builtin_types = 'bold',
+      },
+      plugins = {
+        cmp = {
+          match = 'bold',
+          match_fuzzy = 'bold',
+        },
+        lsp = {
+          diagnostic_error = 'bold',
+          diagnostic_info = 'italic',
+          diagnostic_warn = 'bold',
+        },
+        telescope = {
+          match = 'bold',
+        },
+      },
+    })
+
     vim.cmd.colorscheme('tokyonight')
   end,
 }
