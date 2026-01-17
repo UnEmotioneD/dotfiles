401c86090fec9e6918d44272a279897ea9cc1e1b
UnEmotioneD <blackeagle10@icloud.com>
2026-01-17 20:14:45 +0900

    test(nvim): autopairs - comment out nvim-cmp related parts

--- nvim/.config/nvim/lua/josean/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/josean/plugins/nvim-autopairs.lua
@@ -11,9 +11,9 @@ return {
       },
     })
 
-    local cmp = require('cmp')
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    -- local cmp = require('cmp')
+    -- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    -- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
