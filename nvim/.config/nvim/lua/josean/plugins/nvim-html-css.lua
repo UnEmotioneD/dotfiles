e90e51896b24157a9cbee51e0b4750f67fa3fdb9
UnEmotioneD <blackeagle10@icloud.com>
2026-01-17 20:48:13 +0900

    cleanup(nvim): nvim-html-css plugin

--- nvim/.config/nvim/lua/josean/plugins/nvim-html-css.lua
+++ nvim/.config/nvim/lua/josean/plugins/nvim-html-css.lua
@@ -1,10 +1,7 @@
 return {
   'Jezda1337/nvim-html-css',
   opts = {
-    enable_on = {
-      'html',
-      'jsx',
-    },
+    enable_on = { 'html', 'jsx' },
     handlers = {
       definition = {
         bind = 'gd',
@@ -19,9 +16,6 @@ return {
     documentation = {
       auto_show = true,
     },
-    style_sheets = {
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.3/css/bulma.min.css',
-    },
+    style_sheets = {},
   },
 }
